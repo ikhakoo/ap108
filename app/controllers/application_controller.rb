8d8cac4856f72309d5de96fcb9da92352bf3bdff
ikhakoo <ikhakoo@gmail.com>
2015-06-27 19:48:18 -0400

    bare shoppe app

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  helper_method :current_order, :has_order?
+  
+ private
+
+  def current_order
+    @current_order ||= begin
+      if has_order?
+        @current_order
+      else
+        order = Shoppe::Order.create(:ip_address => request.ip)
+        session[:order_id] = order.id
+        order
+      end
+    end
+  end
+
+  def has_order?
+    !!(
+      session[:order_id] &&
+      @current_order = Shoppe::Order.includes(:order_items => :ordered_item).find_by_id(session[:order_id])
+    )
+  end
+
+  
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  mount Shoppe::Engine => "/shoppe"
+  
+  get "product/:permalink", to: "products#show", as: "product"
+  post "product/:permalink", to: "products#buy", as: "buy"
+  root to: "products#index"
+
+  get "basket", to: "orders#show"
+  delete "basket", to: "orders#destroy"
+
+  match "checkout", to: "orders#checkout", as: "checkout", via: [:get, :patch]
+  match "checkout/pay", to: "orders#payment", as: "checkout_payment", via: [:get, :post]
+  match "checkout/confirm", to: "orders#confirmation", as: "checkout_confirmation", via: [:get, :post]
+
+
+end
